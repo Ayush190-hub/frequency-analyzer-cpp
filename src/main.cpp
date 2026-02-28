eb8cb012ffc30da23a5036eb82e43af456be4dac
Ayush190-hub <ayusharyan162@gmail.com>
2026-02-28 18:22:39 +0530

    added text preprocessing (lowercase + punctuation removal)

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
+string preprocessText (string text) {
+    string cleaned = " ";
+
+    for (char ch: text) {
+        if (isalpha(ch) || isspace(ch)) {
+            cleaned += tolower(ch);
+        }
+    }
+    return cleaned;
+}
+
 int main() {
-    cout << "===== Frequency analyzer project initialized EXIT=====\n" << endl;
+    cout << "===== Frequency analyzer project initialized EXIT =====\n" << endl;
     cout << "Enter text (type EXIT on a new line to stop):\n\n";
 
     string input;
     string line;
     string fullText;
+
+    string cleanedText =
+    preprocessText(fullText);
+    cout << "\n----- Cleaned Text -----\n";
+    cout << cleanedText << endl;
 
     while (true) {
         getline(cin, line);
@@ -17,7 +34,7 @@ int main() {
             break;
         }
 
-        fullText += line + " ";
+        fullText += line + " ";   // This is called Accumulator Pattern
     }
 
     cout << "\n-----  Raw Input collected-----\n";
