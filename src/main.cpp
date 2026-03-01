93f51f5a7cf870818968103fa80ea71f51187e95
Ayush190-hub <ayusharyan162@gmail.com>
2026-03-01 19:19:34 +0530

    Implemented word splitting using using stringstream

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,43 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <sstream>
+#include <vector>
+
 using namespace std;
 
-string preprocessText (string text) {
-    string cleaned = " ";
+string preprocessText(string text) {
+    string cleaned = "";
 
-    for (char ch: text) {
+    for (char ch : text) {
         if (isalpha(ch) || isspace(ch)) {
             cleaned += tolower(ch);
         }
     }
+
     return cleaned;
 }
 
+vector<string> splitWords(string text) {
+    vector<string> words;
+    stringstream ss(text);
+    string word;
+
+    while (ss >> word) {
+        words.push_back(word);
+    }
+
+    return words;
+}
+
 int main() {
-    cout << "===== Frequency analyzer project initialized EXIT =====\n" << endl;
+    cout << "===== Frequency Analyzer Project =====\n";
     cout << "Enter text (type EXIT on a new line to stop):\n\n";
 
-    string input;
     string line;
     string fullText;
 
-    string cleanedText =
-    preprocessText(fullText);
-    cout << "\n----- Cleaned Text -----\n";
-    cout << cleanedText << endl;
-
+    // 1️⃣ Collect Input
     while (true) {
         getline(cin, line);
 
@@ -34,11 +45,27 @@ int main() {
             break;
         }
 
-        fullText += line + " ";   // This is called Accumulator Pattern
+        fullText += line + " ";
     }
 
-    cout << "\n-----  Raw Input collected-----\n";
+    // 2️⃣ Print Raw Input
+    cout << "\n----- Raw Input Collected -----\n";
     cout << fullText << endl;
+
+    // 3️⃣ Preprocess Text
+    string cleanedText = preprocessText(fullText);
+
+    cout << "\n----- Cleaned Text -----\n";
+    cout << cleanedText << endl;
+
+    // 4️⃣ Split Words
+    vector<string> words = splitWords(cleanedText);
+
+    // 5️⃣ Print Words
+    cout << "\n----- Words Extracted -----\n";
+    for (const string &w : words) {
+        cout << w << endl;
+    }
 
     return 0;
 }
